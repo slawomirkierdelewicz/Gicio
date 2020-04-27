3cb4c1e510a6d061f70751efeffbea9b27fdd4ec
Slawomir Kierdelewicz <slawomir.kierdelewicz@cfteurope.pl>
2020-04-27 12:21:02 +0200

    2

--- Gicio/Program.cs
+++ Gicio/Program.cs
@@ -7,6 +7,12 @@ namespace Gicio
         static void Main(string[] args)
         {
             Console.WriteLine("1");
+            Console.WriteLine("master");
+            Console.WriteLine("master");
+            Console.WriteLine("master");
+            Console.WriteLine("master");
+            Console.WriteLine("master");
+        
         }
     }
 }
